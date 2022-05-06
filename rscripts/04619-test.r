b1652981c1c09417d4e123b451f6f18734527c37
marcelovmaciel <marcelovmaciel@protonmail.com>
2022-05-06 16:59:34 -0700

    dont wanna lose this shit

---
+++ rscripts/04619-test.r
@@ -0,0 +1,469 @@
+library("BayesMallows")
+library("dplyr")
+library("ggraph")
+library("haven")
+library("igraph")
+library("magrittr")
+library("purrr")
+##library("tidyr")
+library("tidytable")
+library("votevizr")
+library("xtable")
+## library(reshape2)
+library("ggpubr")
+
+df <- read_sav("../04619/04619.SAV")
+
+
+load("bmm_rank_index.RData")
+
+
+## * p2, who_Iwill_vote_for
+df[, "p2"] -> p2df
+
+## * ★★★ p3b, whos_my_second_if_uncertain
+## Thiswill require some munging
+df[, "p3a"] |> unique()
+df[, "p3b"] |> unique()
+
+
+
+## * p5"s", those_I_reject
+acc <- vector()
+df[,grepl("p5", names(df))] %>%
+  apply(., 1,as.vector) -> p5s
+
+for (unit in 1:dim(p5s)[2]){
+  acc[unit]<- list(p5s[,unit])
+}
+
+those_i_rejects <-  map(acc, ~.x[!is.na(.x)])
+
+## * p6, the pairwise comparisons!
+p6df <- df[, grepl("p6", names(df))]
+
+## * p7, who_will_win
+p7df <- df[, "p7"]
+
+
+## * p10, on_lula
+p10df <- df[, "p10"]
+
+## *  concat data
+
+cbind(p2df,
+  p6df,
+  p7df,
+  p10df) -> subset_df
+
+foofn <- function (x) {
+  myl <- length(subset_df$p6b)
+  foolish <- round(table(subset_df[,x])/myl,2)
+  foolish
+
+}
+
+global_pairwise_comparisons <- map(names(p6df), foofn)
+
+# preprocessing pairs
+subset_df <- rename(subset_df,
+                    who_i_votes_for = p2,
+                    who_will_win = p7,
+                    on_lula = p10,
+                    pair_36 = p6a,
+                    pair_69 = p6b,
+                    pair_39 = p6c,
+                    pair_56 = p6d,
+                    pair_59 = p6e,
+                    pair_35 = p6f
+                    )
+
+subset_df |> names()
+
+get_pair <- function(indx, x, col, pair){
+  if (is.element(x[,col], pair)){
+  tibble(assessor = indx,
+         bottom_item = setdiff(pair, x[,col]),
+         top_item = x[,col])}
+}
+
+get_agent_pairs <- function(indx,x){
+  ## too lazy to discover how to write a map over a zip in R
+  pair_36 <- get_pair(indx, x, "pair_36", c(3,6))
+  pair_69 <- get_pair(indx, x, "pair_69", c(6,9))
+  pair_39 <- get_pair(indx, x, "pair_39", c(3,9))
+  pair_56 <- get_pair(indx, x, "pair_56", c(5,6))
+  pair_59 <- get_pair(indx, x, "pair_59", c(5,9))
+  pair_35 <- get_pair(indx, x, "pair_35", c(3,5))
+
+  bind_rows(pair_36,
+        pair_69,
+        pair_39,
+        pair_56,
+        pair_59,
+        pair_35)
+}
+
+crazies_missing <- vector()
+
+for (i in 1:nrow(subset_df)) {
+  foo <- get_agent_pairs(i, subset_df[i, ])
+  if(length(foo) ==0)  {crazies_missing <- c(crazies_missing, i)}
+  if(length(foo) ==0) next
+}
+
+
+
+subset_df %>%
+  filter(!(is.element(row_number(), crazies_missing))) -> subset_df
+
+
+# https://stackoverflow.com/questions/55091438/r-igraph-find-all-cycles
+FindCycles = function(g) {
+    Cycles = NULL
+    for(v1 in V(g)) {
+        if(degree(g, v1, mode="in") == 0) { next }
+        GoodNeighbors = neighbors(g, v1, mode="out")
+        GoodNeighbors = GoodNeighbors[GoodNeighbors > v1]
+        for(v2 in GoodNeighbors) {
+            TempCyc = lapply(all_simple_paths(g, v2,v1, mode="out"), function(p) c(v1,p))
+            TempCyc = TempCyc[which(sapply(TempCyc, length) > 3)]
+          TempCyc = TempCyc[sapply(TempCyc, min) == sapply(TempCyc, `[`, 1)]
+          Cycles  = c(Cycles, TempCyc)
+        }
+    }
+    Cycles
+}
+
+
+acc_cyclic <- vector()
+for (i in 1:nrow(subset_df)) {
+  foo <- get_agent_pairs(i, subset_df[i, ])
+  actors <- data.frame(name = c(3,5,6,9))
+  relations <- data.frame(from = foo$top_item,
+                        to = foo$bottom_item)
+  g<- graph_from_data_frame(relations, directed = TRUE, vertices = actors)
+
+  acc_cyclic <- c(acc_cyclic, length(FindCycles(g)) > 0 )
+}
+
+
+noncyclic_agents <- subset_df[!acc_cyclic,]
+
+noncyclic_agents %>% dim
+
+
+acc_pairs <-   tibble(assessor = numeric(),
+         bottom_item = numeric(),
+         top_item = numeric())
+
+for (i in 1:nrow(noncyclic_agents)) {
+  acc_pairs <- bind_rows(acc_pairs,
+            get_agent_pairs(i, noncyclic_agents[i,]))
+}
+
+## there is some bullshit about labeling here
+## Accordinly, I'll relabe this to 1:4
+
+acc_pairs$bottom_item %<>%
+  as.factor(.) %>%
+  recode_factor(.,
+              `3`= 1,
+              `5` = 2,
+              `6` = 3,
+              `9` = 4) %>%
+  as.numeric(.)
+
+
+
+
+acc_pairs$top_item %<>%
+  as.factor(.) %>%
+  recode_factor(.,
+              `3`= 1,
+              `5` = 2,
+              `6` = 3,
+              `9` = 4) %>%
+  as.numeric(.)
+
+
+tc <- generate_transitive_closure(acc_pairs)
+
+init_rank <- generate_initial_ranking(tc)
+
+
+bmm_test <- compute_mallows(rankings = init_rank,
+                             preferences = tc, save_aug = TRUE)
+
+
+bmm_test$burnin <- 1000
+
+# compute_posterior_intervals(bmm_test,parameter = "rho")
+
+#plot(bmm_test, parameter = "rho", items = 1:4)
+## Rtilde = latent ranking vector
+
+
+convergence<- assess_convergence(bmm_test,
+                   parameter = "rho",
+                   items = c(1, 2,3,4),
+                   assessors = 1:8)
+
+##compute_posterior_intervals(bmm_test, parameter= "rho")
+
+
+##ggsave("convergence.png")
+
+
+lastiter <- filter(bmm_test$augmented_data, iteration == 2000)
+
+#foo <- compute_consensus(bmm_test, type = "MAP")
+
+#plot(bmm_test , parameter = "rho")
+
+
+## how many agents are unstable?
+
+lastiter %>%
+  group_by(assessor) %>%
+  arrange(.,value,  .by_group = TRUE) %>%
+  summarise(ranking = item) %>%
+  mutate(ranking = recode(ranking,
+                          `Item 1`= 1,
+                          `Item 2` = 2,
+                          `Item 3` = 3,
+                          `Item 4` = 4))  %>%
+  group_by(assessor) %>%
+  group_split()  %>%
+  lapply(.,  function (x) {x$ranking}) %>%
+  do.call(rbind, .) ->
+  last_rank
+
+
+lastiter %>%
+  group_by(assessor) %>%
+  summarise(ranking = value) %>%
+  group_split()  %>%
+  lapply(.,  function (x) {x$ranking}) %>%
+  do.call(rbind, .) -> last_indexes
+
+save(bmm_test, last_rank, last_indexes, file="bmm_rank_index.RData")
+
+last_indexes
+## DONE: understand wtf is going on here
+## OK, init_rank order the following way:
+## names(init_rank) == c(alternative1,alternative2, ....)
+## so its values are the INDEXES of each alternative
+## on the other hand last_rank as I've applied lists the order of alternatives
+## at each row. So, it is an ACTUAL ranking.
+
+acc_borda_result <- vector()
+
+## last_indexes %>% head
+
+## 1/last_indexes %>% head
+
+## colSums(1/last_indexes)
+
+# rev(last_rank)
+
+# match(4,rev(last_rank[1,]))
+
+for (i in 1:4){
+  apply(last_rank,
+        1,
+        function (x) {match(i,rev(x))}) %>%
+    sum -> borda_result
+  acc_borda_result <- c(acc_borda_result,
+                        borda_result)
+}
+
+acc_borda_result
+
+last_rank %>% head
+last_indexes %>% head
+
+acc_borda_result
+
+# Show this 
+acc_pairs %>%
+  group_by(assessor) %>%
+  group_size %>%
+  table   %>% data.frame %>%
+xtable(.,  type = "latex", tabular.environment = "longtable")
+
+
+
+acc_pairs %>%
+  group_by(assessor) %>%
+  group_size %>%
+  table   %>% data.frame
+
+apply(init_rank == last_indexes, 1, all) |> sum()
+
+get_borda_from_indexes <- function(x){
+4*tabulate(x[,1]) +
+3*tabulate(x[,2]) +
+2*tabulate(x[,3]) +
+1*tabulate(x[,4])
+}
+
+acc_borda_result
+
+# FIXME: this is leads to an error. tell them!!!!
+freq_ranks <- rank_freq_distr(last_indexes)
+freq_ranks<- freq_ranks[order(freq_ranks[,5], decreasing = TRUE),]
+
+freq_ranks_again <- rank_freq_distr(last_rank)
+freq_ranks_again <- freq_ranks_again[order(freq_ranks_again[,5], decreasing = TRUE),]
+
+freq_ranks_again %>%
+  data.frame -> test
+
+names(test) <- c(1,2,3,4,"freq")
+
+
+for (i in 1:4){
+test[,i] %>%
+  as.factor %>%
+  recode_factor(.,
+              `1`= "ciro",
+              `2` = "haddad",
+              `3` = "alckmin",
+              `4` = "bolsonaro") -> test[,i]
+}
+
+
+test_rankings <- test
+
+test_indexes <- last_indexes %>% data.frame
+
+
+names(test_indexes) <- c("ciro", "haddad", "alckmin", "bolsonaro")
+
+
+fn_that_should_be_anonymous <-  function (candidatename) {
+test_indexes %>% ggplot(
+aes(x = .data[[candidatename]])) +
+  geom_bar(aes( fill = .data[[candidatename]]  ),
+           position = position_dodge()) + theme_bw() }
+
+# R doesn't fucking understand pointers. Outrageous.
+ciroplot <- fn_that_should_be_anonymous("ciro")
+haddadplot <- fn_that_should_be_anonymous("haddad")
+alckminplot <- fn_that_should_be_anonymous("alckmin")
+bozoplot <- fn_that_should_be_anonymous("bolsonaro")
+
+
+position_counts <- ggarrange(ciroplot, haddadplot, alckminplot, bozoplot , ncol = 2, nrow = 2)
+
+
+
+last_rank_df <- data.frame(last_rank)
+
+
+
+names(last_rank_df) <- c("choice1", "choice2", "choice3", "choice4")
+
+for (i in c("choice1", "choice2", "choice3", "choice4")) {
+last_rank_df[,i] %>% as.factor %>%
+  recode_factor(.,
+              `1`= "ciro",
+              `2` = "haddad",
+              `3` = "alckmin",
+              `4` = "bolsonaro") -> last_rank_df[,i]
+}
+
+ fn_that_should_be_anonymous2 <- function (colname) {
+last_rank_df %>% ggplot(
+aes(x = .data[[colname]])) +
+  geom_bar(aes( fill = .data[[colname]]  ),
+           position = position_dodge()) + theme_bw() }
+
+choice1df <- fn_that_should_be_anonymous2("choice1")
+choice2df <- fn_that_should_be_anonymous2("choice2")
+choice3df <- fn_that_should_be_anonymous2("choice3")
+choice4df <- fn_that_should_be_anonymous2("choice4")
+
+myplt2 <- ggarrange(choice1df, choice2df, choice3df, choice4df, ncol = 2,
+                    nrow = 2)
+
+
+## test_rankings %>% mutate(prop = formattable::percent(freq / sum(freq)))
+
+
+## test_rankings %>% head
+
+
+## test_indexes %>% head
+
+
+
+
+xtable(test,  type = "latex", tabular.environment = "longtable", digits = 0)
+
+
+
+# TODO: show them the error
+v<- test
+
+acc_first_round <- vector()
+
+for (i in c("ciro", "haddad", "alckmin", "bolsonaro")){
+acc_first_round<- c(acc_first_round,(v[,5][v[,1] == i]|> sum()))
+}
+
+
+sanity_check <- data.frame(acc_first_round)
+
+sanity_check$candidate <- c("ciro", "haddad", "alckmin", "bolsonaro")
+
+sanity_check<- sanity_check[order(sanity_check$acc_first_round, decreasing =TRUE),]
+
+xtable(sanity_check,  type = "latex", tabular.environment = "longtable", digits = 0)
+
+
+v[v[,1] == 4]
+freq_ranks[,1]
+table(last_rank[,1])
+
+# FIXME: this should match the acc_borda_result
+get_borda_from_indexes(last_indexes)
+
+
+## 4*tabulate(init_rank[,1]) +
+## 3*tabulate(init_rank[,2]) +
+## 2*tabulate(init_rank[,3]) +
+## 1*tabulate(init_rank[,4])
+
+
+
+actors <- data.frame(name = c("ciro","haddad","alckmin","bolsonaro"))
+relations <- data.frame(from = c("ciro",
+                                 "ciro",
+                                 "ciro",
+                                 "alckmin",
+                                 "alckmin",
+                                 "bolsonaro"),
+                        to = c("alckmin",
+                               "bolsonaro",
+                               "haddad",
+                               "bolsonaro",
+                               "haddad",
+                               "haddad"))
+
+relations
+
+g<- graph_from_data_frame(relations, directed = TRUE, vertices = actors)
+
+pl<- ggraph(g, layout = 'graphopt') +
+    geom_edge_link(aes(start_cap = label_rect(node1.name), end_cap = label_rect(node2.name)),
+                   arrow = arrow(type = "closed", length = unit(3, 'mm'))) +
+    geom_node_text(aes(label = name)) + theme_graph()
+
+ggsave("cw.png")
+
+
+
+map(global_pairwise_comparisons,
+    ~xtable(.x,  type = "latex", tabular.environment = "longtable"))
