c5ff23e4d14d4b432e1859ca148f282b9859a806
marcelovmaciel <marcelovmaciel@protonmail.com>
2022-06-28 13:22:05 -0700

    CHORE: fixing the triangle. now it is equilateral

--- CWElectionsBR/test/geometry_tests.jl
+++ CWElectionsBR/test/geometry_tests.jl
@@ -8,10 +8,25 @@ using GLMakie
 using Combinatorics
 import Meshes
 
-externalpoints = GeometryBasics.Point2.([(0.,0.), (1.,0.), (0.5,1.)])
-midpoints = GeometryBasics.Point2.([(0.5, 0.0), (0.25, 0.5 ), (0.75, 0.5)])
 
-propercombinations = [[i,j,GeometryBasics.Point2([0.5, 0.33])] for j in externalpoints
+tern2cart(a, b, c) = (1 / 2 * (2b + c) / (a + b + c), √3 / 2 * (c / (a + b + c)))
+
+
+plainpoints = [(0.,0.), (1.,0.), (0.5,sqrt(3)/2)]
+
+midpoint(p1,p2) = ( (p1[1] + p2[1] )/2, (p1[2] + p2[2])/2)
+
+external_triangle_centroid = Meshes.centroid(Meshes.Triangle(plainpoints...)) |> x-> x.coords |> Tuple |> GeometryBasics.Point2
+
+
+externalpoints = GeometryBasics.Point2.(plainpoints)
+
+
+
+midpoints = GeometryBasics.Point2.(map(x->(midpoint(x...)), combinations(plainpoints, 2) ))
+#midpoints = GeometryBasics.Point2.([(0.5, 0.0), (0.25, 0.5 ), (0.75, 0.5)])
+
+propercombinations = [[i,j,external_triangle_centroid] for j in externalpoints
                         for i in midpoints]
 
 # Meshes.centroid(Meshes.Triangle((0.,0.), (1.,0.), (0.5,1.)))
@@ -22,10 +37,12 @@ barepoints = map(x-> x.coords,map(point -> Meshes.centroid(Meshes.Triangle(map(x
 
 
 foo = poly(mypoly,
-        color = :red, strokecolor = :black, strokewidth = 1)
+        color = :white, strokecolor = :black, strokewidth = 1)
 
-weirdcentroid(x,y)= ((y <= 0.45) && (y>=0.25)) && ((x <= 0.6) && (x>=0.4))
+weirdcentroid(x,y)= ((y <= 0.4) && (y>=0.2)) && ((x <= 0.6) && (x>=0.4))
 weirdcentroid(x) = weirdcentroid(x[1], x[2])
+
+#scatter!(barepoints) 
 
 scatter!(filter(!weirdcentroid,barepoints))  
 
@@ -35,8 +52,14 @@ hideydecorations!(foo.axis)
 
 text!("A", position = (-0.03,-0.01))
 text!("B", position = (1.01,-0.01))
-text!("C", position = (0.49,1))
+text!("C", position = (0.49,0.8655))
 
 
 
 
+
+
+
+
+poly(Polygon(externalpoints),
+        color = :red, strokecolor = :black, strokewidth = 1)
