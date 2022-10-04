f4bb52da5376a7d60e78c6a4bec1b10b38bbd3cf
marcelovmaciel <marcelovmaciel@gmail.com>
2022-10-04 12:41:19 -0700

    ultimas modificacoes na correcao de frequencias

---
+++ rscripts/04169-correctingsurvey.R
@@ -0,0 +1,76 @@
+library(xtable)
+load("./dta_objects/freq_ranks_inferred.RData")
+
+library("roperators")
+library("dplyr")
+
+freq_ranks_inferred %>% summarize(foo = sum(freq))
+
+sum(freq_ranks_inferred[,"freq"])
+
+## 2937 -> 92
+## x -> 8
+
+
+
+
+freq_ranks_inferred
+
+
+freq_ranks_inferred[freq_ranks_inferred[,"1"] == "alckmin",]
+
+corrected_freq_ranks <- tibble(freq_ranks_inferred)
+
+print(corrected_freq_ranks, n = 29)
+
+last_row <- tribble(~`1` ,~`2`,~`3`,~`4`, ~freq,
+  "other", "other", "other", "other", 228)
+
+corrected_freq_ranks<- rbind(corrected_freq_ranks, last_row, stringsAsFactors = FALSE)
+
+corrected_freq_ranks %>%
+  group_by(`1`) %>%
+  summarise(cnt = sum(freq)) %>% mutate(prop = cnt / sum(cnt))
+
+
+## transfer chab to hcab
+corrected_freq_ranks[5, "freq"] %-=% 70
+corrected_freq_ranks[2,"freq"] %+=% 70
+
+## cbah to bcah
+corrected_freq_ranks[17, "freq"] %-=% 50
+corrected_freq_ranks[2, "freq"] %+=% 50
+
+## cabh to bcah
+corrected_freq_ranks[15, "freq"] %-=% 66
+corrected_freq_ranks[3, "freq"] %+=% 66
+
+
+## transfer abch to bach
+corrected_freq_ranks[14, "freq"] %-=% 82
+corrected_freq_ranks[1, "freq"] %+=% 82
+
+## transfer abhc to bach
+corrected_freq_ranks[19, "freq"] %-=% 34
+corrected_freq_ranks[7, "freq"] %+=% 34
+
+## transfer acbh to bach
+corrected_freq_ranks[13, "freq"] %-=% 80
+corrected_freq_ranks[1, "freq"] %+=% 80
+
+## remove from achb
+corrected_freq_ranks[9, "freq"] %-=% 120
+
+corrected_freq_ranks <- corrected_freq_ranks[order(corrected_freq_ranks[,"freq"],
+                          decreasing = TRUE),]
+
+print(corrected_freq_ranks, n=29)
+
+corrected_freq_ranks %>%
+  group_by(`1`) %>%
+  summarise(cnt = sum(freq)) %>% mutate(prop = cnt / sum(cnt))
+
+save(corrected_freq_ranks,
+     file="./dta_objects/corrected_freq_ranks.RData")
+
+# load("./dta_objects/corrected_freq_ranks.RData")o
---
+++ rscripts/04619-correctedFreqs_results.R
@@ -0,0 +1,137 @@
+library("dplyr")
+library(purrr)
+library("magrittr")
+load("./dta_objects/corrected_freq_ranks.RData")
+# load("./dta_objects/freq_ranks_inferred.RData")
+
+## last_row <- tribble(~`1` ,~`2`,~`3`,~`4`, ~freq,
+##   "other", "other", "other", "other", 228)
+
+## freq_ranks_inferred <-  rbind(freq_ranks_inferred, last_row, stringsAsFactors = FALSE)
+
+##  freq_ranks_inferred %>%
+##   group_by(`1`) %>%
+##   summarise(cnt = sum(freq))  %>%
+##   mutate(prop = cnt / sum(cnt)) %>%
+##   arrange(desc(prop))
+
+print(corrected_freq_ranks, n = 29)
+
+
+corrected_freq_ranks %>%
+  group_by(`1`) %>%
+  summarise(cnt = sum(freq))  %>%
+  mutate(prop = cnt / sum(cnt)) %>%
+  arrange(desc(prop)) -> plurality_table
+
+corrected_freq_ranks %>%
+  group_by(`4`) %>%
+  summarise(cnt = sum(freq))  %>%
+  mutate(prop = cnt / sum(cnt)) %>%
+  arrange(prop) -> anti_plurality_table
+
+corrected_freq_ranks %>%
+  group_by(`1`) %>%
+  summarise(cnt = sum(freq))  %>%
+  arrange(`1`) -> firsto_table
+
+corrected_freq_ranks %>%
+  group_by(`2`) %>%
+  summarise(cnt = sum(freq))  %>%
+  arrange(`2`) -> secondo_table
+
+corrected_freq_ranks %>%
+  group_by(`3`) %>%
+  summarise(cnt = sum(freq))  %>%
+  arrange(`3`) -> thirdo_table
+
+corrected_freq_ranks %>%
+  group_by(`4`) %>%
+  summarise(cnt = sum(freq))  %>%
+  arrange(`4`) -> quarto_table
+
+
+tibble(quarto_table[,"4"],
+(3 * firsto_table[,"cnt"] +
+ 2 * secondo_table[,"cnt"] +
+ 1 * thirdo_table[,"cnt"] +
+ 0 * quarto_table[,"cnt"])) -> borda_table
+
+get_tally_scores_from_proportions <- function(df,candidate, candidate2) {
+
+df %>%
+  filter(`1` == candidate | `1` == candidate2) %>%
+  group_by(`1`) %>%
+  summarise(cnt = sum(freq)) -> tc1
+
+df %>%
+    filter(., (`1` != candidate & `1` !=  candidate2) &
+            (`2` == candidate | `2` ==  candidate2)) %>%
+  group_by(`2`) %>%
+    summarise(cnt = sum(freq)) -> tc2
+
+df %>%
+filter(., (`1` != candidate & `1` !=  candidate2) &
+            (`2` != candidate & `2` !=  candidate2) &
+            (`3` == candidate | `3` ==  candidate2)) %>%
+  group_by(`3`) %>%
+  summarise(cnt = sum(freq)) -> tc3
+
+  tibble(tc1[,"1"],
+       tc1[,"cnt"] +
+       tc2[,"cnt"] +
+       tc2[,"cnt"]) -> tally
+
+  tally %<>% mutate(prop = cnt / sum(cnt) )
+  return(tally)
+  }
+
+
+get_tally_winner <- function(df,c1,c2){
+
+tw <- (get_tally_scores_from_proportions(corrected_freq_ranks,
+                                  c1,
+                                  c2) %>% filter(cnt == max(cnt)))[1,"1"]
+  return(as.character(tw[[1]]))
+}
+
+get_tally_winner(corrected_freq_ranks,
+                                  "bolsonaro",
+                                  "haddad")
+
+get_tallies_winners <- function (x,df) {
+  candidates <- c("alckmin", "haddad", "bolsonaro", "ciro")
+
+  othercandidates <- candidates[candidates != x]
+
+  acc <- list()
+  for (i in othercandidates){
+    acc <- c(acc,get_tally_winner(df,x,i))
+  }
+  return(flatten_chr(acc))
+}
+
+bar <- get_tallies_winners("bolsonaro", corrected_freq_ranks)
+
+bar
+
+
+get_tally_scores_from_proportions(corrected_freq_ranks,"bolsonaro","haddad")
+get_tally_scores_from_proportions(corrected_freq_ranks, "bolsonaro","ciro")
+get_tally_scores_from_proportions(corrected_freq_ranks, "bolsonaro","alckmin")
+
+get_tally_scores_from_proportions(corrected_freq_ranks,"haddad","bolsonaro")
+get_tally_scores_from_proportions(corrected_freq_ranks, "haddad","ciro")
+get_tally_scores_from_proportions(corrected_freq_ranks, "haddad","alckmin")
+
+get_tally_scores_from_proportions(corrected_freq_ranks,"ciro","haddad")
+get_tally_scores_from_proportions(corrected_freq_ranks, "ciro","bolsonaro")
+get_tally_scores_from_proportions(corrected_freq_ranks, "ciro","alckmin")
+
+
+
+
+# FIXME: check if anything changed here
+corrected_freq_ranks %>% summarise(cnt = sum(freq))
+
+# Bolsonaro is the borda winner, but not the condorcet winner !!!
