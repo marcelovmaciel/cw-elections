910013158032583ed9cb3ff55d87c56d8e819434
marcelovmaciel <marcelovmaciel@gmail.com>
2022-11-26 00:02:10 -0800

    almost done with the transfer algorithm

--- CWElectionsBR/test/transfer-votes-algoritm.jl
+++ CWElectionsBR/test/transfer-votes-algoritm.jl
@@ -9,8 +9,6 @@ using RCall
 
 @rlibrary rankdist 
 
-
-
 reval("load('../rscripts/dta_objects/freq_ranks_inferred.RData')")
 @rget freq_ranks_inferred
 
@@ -50,63 +48,65 @@ overvoted = prop_df[prop_df[!, :prop] .> prop_df[!, :actual_proportions],:]
 
 undervoted = prop_df[prop_df[!, :prop] .< prop_df[!, :actual_proportions],:]
 
-ideological_ordering = ["haddad", "ciro", "alckmin", "bolsonaro"]
-
-over1 = overvoted[1,:][1]
-
-under1 = undervoted[1,:][1]
-
-over1_freq_ranks  = filter(x-> x[:1] == over1, freq_ranks_inferred)
-
-under1_freq_ranks  = filter(x-> x[:1] == under1, freq_ranks_inferred)
-
-findfirst(x-> x == under1, Vector(over1_freq_ranks[1,:][1:4]))
-
-over1_freq_ranks[!, :dist_to_under] = [(findfirst(x-> x == under1, Vector(j[1:4]))-1) for j in eachrow(over1_freq_ranks)]
-
-under1_freq_ranks[!, :dist_to_over] = [(findfirst(x-> x == over1, Vector(j[1:4]))-1) for j in eachrow(under1_freq_ranks)]
-
-#prop_df[!,:dist_proportion] = 
 
 prop_df[!, :dist_proportion] = (prop_df[!,:prop] .- prop_df[!,:actual_proportions] .|>
 x-> round(x, digits = 3) .|> 
  abs)
 
 total_tallies = sum(prop_df[!, :freq_sum])
-
 prop_df[!, :dist_freqs] = round.(prop_df[!,:dist_proportion] .*  total_tallies, digits = 0)
 
-under1_needs = Int(first(filter(x-> x[:1] == under1, prop_df)[!,:dist_freqs]))
-over1_can_give = Int(first(filter(x-> x[:1] == over1, prop_df)[!,:dist_freqs]))
+over1 = overvoted[2,:][1]
+
+overvoted
 
 
-over1_freq_ranks = sort(over1_freq_ranks, :dist_to_under)
-
-last_giving_index = findfirst(x->x>= over1_can_give, cumsum(over1_freq_ranks[!,:freq]))
-
-receiving_rankings = copy(under1_freq_ranks)
-
-over1_freq_ranks
+under1 = undervoted[1,:][1]
 
 
-giving_rankings
 
+
+function preprocess_over_under(freq_ranks_inferred, prop_df, overcandidate, undercandidate)
+    over_freq_ranks  = filter(x-> x[:1] == overcandidate, freq_ranks_inferred)
+    under_freq_ranks  = filter(x-> x[:1] == undercandidate, freq_ranks_inferred)
+
+    over_freq_ranks[!, :dist_to_under] = [(findfirst(x-> x == undercandidate, Vector(j[1:4]))-1) for j in eachrow(over_freq_ranks)]
+    under_freq_ranks[!, :dist_to_over] = [(findfirst(x-> x == overcandidate, Vector(j[1:4]))-1) for j in eachrow(under_freq_ranks)]
+    
+
+    over_can_give = Int(first(filter(x-> x[:1] == overcandidate, prop_df)[!,:dist_freqs]))    
+    under_needs = Int(first(filter(x-> x[:1] == undercandidate, prop_df)[!,:dist_freqs]))
+
+    over_freq_ranks = sort(over_freq_ranks, :dist_to_under)
+    return(over_freq_ranks = over_freq_ranks,
+           under_freq_ranks = under_freq_ranks,
+           over_can_give = over_can_give,
+           under_needs = under_needs)
+end
+
+foo = preprocess_over_under(freq_ranks_inferred,prop_df,over1,under1)
+
+
+
+
+
+
+#last_giving_index = findfirst(x->x>= over_can_give, cumsum(over_freq_ranks[!,:freq]))
 
 function dist_sourcerank_targetranks(sourcerank, targetranks)
 [ Int(rcopy(DistancePair(Vector(sourcerank[1:4]),j)))
-for j in  (map(x-> Vector(x[1:4]), eachrow(targetranks)))]
+for j in  (map(x-> Vector(x[1:4]), eachrow(targetranks)))] 
 end
 
-
 function find_targetRankIndex_given_sourceRank(source_rank, target_ranks)
-    dist_sourcerank_targetranks(source_rank, target_ranks) |> x-> findfirst(foo-> foo == 1, x)    
+    dist_sourcerank_targetranks(source_rank, target_ranks) |> argmin  
 end    
 
 
 
 function transfer_rankings(source_rankings, target_rankings, can_give, must_receive)
     given = 0
-    needs_to_give = can_give
+    needs_to_give = copy(can_give)
     source_rankings_copy = copy(source_rankings)
     target_rankings_copy = copy(target_rankings)
     for source_rank in eachrow(source_rankings_copy)
@@ -116,27 +116,46 @@ function transfer_rankings(source_rankings, target_rankings, can_give, must_rece
         else 
             will_give = row_can_give
         end
+        println(source_rank)
+        println(target_rankings_copy)
         target_index = find_targetRankIndex_given_sourceRank(source_rank, target_rankings_copy)
         source_rank[:freq] -= will_give 
+        println(target_index)
         target_rankings_copy[target_index, :freq] += will_give
         given += will_give 
         needs_to_give -= will_give
     end
     yet_needed = must_receive - given 
-    return(updated_source = source_rankings_copy, updated_target = target_rankings_copy, yet_needed = yet_needed)
+    source_can_still_give = can_give - given 
+    return(updated_source = source_rankings_copy, 
+    updated_target = target_rankings_copy,
+     yet_needed = yet_needed,
+     source_can_still_give = source_can_still_give)
 end
 
-giving_rankings
-
-bar = transfer_rankings(over1_freq_ranks,
-    receiving_rankings,
-    over1_can_give,
-    under1_needs)
 
 
-sum(bar.updated_source[!,:freq])/total_tallies
+foo.over_freq_ranks
 
 
-over1_freq_ranks
-giving_rankings
-receiving_rankings
+bar = transfer_rankings(foo.over_freq_ranks,
+                        foo.under_freq_ranks,
+                        foo.over_can_give,
+                        foo.under_needs)
+
+
+
+
+foo.over_can_give                        
+
+foo.under_needs 
+
+
+bar.yet_needed                        
+
+bar.updated_source
+                        
+bar.source_can_still_give                        
+                        
+sum(bar.updated_target[!,:freq])/total_tallies
+
