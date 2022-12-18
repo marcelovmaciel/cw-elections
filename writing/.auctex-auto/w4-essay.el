2013faebc5760322b3dff49591e3f0969077515e
marcelovmaciel <marcelovmaciel@gmail.com>
2022-12-17 17:48:24 -0800

    new table/plot

--- writing/.auctex-auto/w4-essay.el
+++ writing/.auctex-auto/w4-essay.el
@@ -5,13 +5,13 @@
                      '(("article" "hidelinks" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "a4paper" "width=150mm" "top=25mm" "bottom=25mm") ("biblatex-chicago" "authordate" "strict" "backend=biber" "bibencoding=inputenc")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -43,6 +43,7 @@
     "tbl:subtab1"
     "tbl:subtab2"
     "tbl:tab1"
+    "fig:positional4c"
     "fig:notac1"
     "fig:notbc1"
     "fig:notah1"
