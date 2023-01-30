cfa4adfd84cfe24c15db0d140d3cef631513c21e
marcelovmaciel <marcelovmaciel@gmail.com>
2023-01-30 10:54:08 -0800

    mapped freq_ranks_inferred to all imputted files

--- CWElectionsBR/test/02-make_freq_ranks_inferred.jl
+++ CWElectionsBR/test/02-make_freq_ranks_inferred.jl
@@ -8,39 +8,48 @@ import Base.Filesystem as fl
 using Pipe 
 
 
-
 dfspath = "../rscripts/dfs/"
 
-simpler_rankings = cw.CSV.read(dfspath * "simpler_rankings.csv", cw.DataFrame)
+#simpler_rankings = cw.CSV.read(dfspath * "simpler_rankings.csv", cw.DataFrame)
 
-orderings = @pipe (
+function make_freq_ranks_inferred(df)
+    orderings = @pipe (
     DataFrame(ranking_vectors = 
     [string.(map(y->findfirst(x->x==y, r), [1,2,3,4]))
-             for r in eachrow(simpler_rankings)]) |>
+             for r in eachrow(df)]) |>
              groupby(_, :ranking_vectors) |>
              combine(_, nrow => :count) |>
               rename(_, :count => :freq) |> 
               sort(_, :freq, rev = true ) )
+    orderings.prop = map(x -> round(x / sum(orderings.freq), digits = 3),
+                        orderings.freq)
+    c1,c2,c3,c4  = map(y->map(x->x[y], orderings.ranking_vectors), 1:4)
+    freq_ranks_inferred = DataFrame(Dict("1"=> c1,
+                                         "2" => c2,
+                                         "3" => c3,
+                                         "4" => c4,
+                                         "freq"=> orderings.freq,
+                                         "prop" => orderings.prop))
+    return(orderings,freq_ranks_inferred)
+end                                     
 
-orderings.prop = map(x -> round(x / sum(orderings.freq), digits = 3),
- orderings.freq)
+dfs_names = readdir(dfspath)
+imputted_dfs = filter(x->occursin("imputted",x), dfs_names)
+
+for imp in imputted_dfs
+    dfname =  fl.splitext(imp)[1]
+    dfpath = joinpath(dfspath,imp)
+    df = cw.CSV.read(dfpath, cw.DataFrame)
+    orderingname = dfname * "_oi.csv"
+    freq_ranks_inferred_name = dfname * "_fri.csv"
+    oi,fri = make_freq_ranks_inferred(df)    
+    cw.CSV.write("../rscripts/dfs/" * orderingname,oi )
+    cw.CSV.write("../rscripts/dfs/" * freq_ranks_inferred_name, fri)
+end    
 
 
-c1,c2,c3,c4  = map(y->map(x->x[y], orderings.ranking_vectors), 1:4)
-
-freq_ranks_inferred = DataFrame(Dict("1"=> c1,
-                                     "2" => c2,
-                                     "3" => c3,
-                                     "4" => c4,
-                                     "freq"=> orderings.freq,
-                                     "prop" => orderings.prop))
-
-freq_ranks_inferred
- 
-cw.CSV.write("../rscripts/dfs/orderings_inferred.csv",orderings )
-
-
-cw.CSV.write("../rscripts/dfs/freq_ranks_inferred.csv",freq_ranks_inferred )
+##cw.CSV.write("../rscripts/dfs/orderings_inferred.csv",orderings )
+##cw.CSV.write("../rscripts/dfs/freq_ranks_inferred.csv",freq_ranks_inferred )
 
 
 
