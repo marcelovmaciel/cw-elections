ae1eb4e6de5cd1602bd681ad546719ad5ba2f77a
marcelovmaciel <marcelovmaciel@gmail.com>
2023-02-06 13:04:07 -0800

    finished avg stuff

--- CWElectionsBR/test/02-point5-avgstd_imputations.jl
+++ CWElectionsBR/test/02-point5-avgstd_imputations.jl
@@ -13,7 +13,7 @@ using RCall
 using Statistics
 @rimport tidyr
 
-df = cw.CSV.read(cw.dfspath * "polyreg1_oi.csv", cw.DataFrame)
+
 
 dfs_names = readdir(cw.dfspath)
 oi_dfs = filter(x->occursin("_oi",x), dfs_names)
@@ -36,5 +36,36 @@ end
 
 avg_pmm = get_avg_std(pmm_oi)
 avg_cart = get_avg_std(cart_oi)
+avg_poly = rename(cw.CSV.read(cw.dfspath * "polyreg1_oi.csv", cw.DataFrame), :freq => :freq_mean, 
+:prop => :prop_mean )
+
+cw.CSV.write("../rscripts/dfs/" * "avgpmmoi.csv", avg_pmm)
+cw.CSV.write("../rscripts/dfs/" * "avgcartoi.csv", avg_cart)
+cw.CSV.write("../rscripts/dfs/" * "avgpolyoi.csv", avg_poly)
+
+baseavg = select(avg_pmm, :ranking_vectors, :freq_mean, :prop_mean)
+
+append!(baseavg, select(avg_cart, :ranking_vectors, :freq_mean, :prop_mean))
+
+append!(baseavg, avg_poly)
+
+meanofmeans = groupby(baseavg, :ranking_vectors) |> 
+x-> combine(x, :freq_mean => mean, :prop_mean => mean)
+
+meanofmeans.freq_mean_mean = map(x->Int(round(x,digits = 0)),
+meanofmeans.freq_mean_mean)
+
+cw.CSV.write("../rscripts/dfs/" * "meanofmeansoi.csv", meanofmeans)
 
 
+c1,c2,c3,c4  = map(y->map(x->x[y], meanofmeans.ranking_vectors), 1:4)
+
+
+freq_ranks_inferred = DataFrame(Dict("1"=> c1,
+                                     "2" => c2,
+                                     "3" => c3,
+                                     "4" => c4,
+                                     "freq"=> meanofmeans.freq_mean_mean,
+                                     "prop" => meanofmeans.prop_mean_mean))
+
+cw.CSV.write("../rscripts/dfs/" * "meanofmeansfri.csv", freq_ranks_inferred)
