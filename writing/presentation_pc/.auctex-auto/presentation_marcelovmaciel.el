4ee75f111bee6ce0034a9472988047eda61cee35
marcelovmaciel <marcelovmaciel@gmail.com>
2023-03-08 17:40:01 -0800

    pc presentation

---
+++ writing/presentation_pc/.auctex-auto/presentation_marcelovmaciel.el
@@ -0,0 +1,35 @@
+(TeX-add-style-hook
+ "presentation_marcelovmaciel"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("ulem" "normalem") ("biblatex" "backend=biber")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "../images/table_counterfactuals"
+    "beamer"
+    "beamer10"
+    "inputenc"
+    "ulem"
+    "float"
+    "caption"
+    "subcaption"
+    "graphicx"
+    "biblatex")
+   (LaTeX-add-labels
+    "fig:saari_nurmi"
+    "fig:rep_ot"
+    "fig:counts"
+    "fig:c1dropping")
+   (LaTeX-add-bibliographies
+    "refs"))
+ :latex)
+
